dd2c0d343e7ab1e12256fb00bdcf2be81b5a0327
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2023-09-14 13:40:09 -0700

    Adding a solution to Exercism exercise

---
+++ Armstrong_Numbers/Armstrong_Numbers.h
@@ -0,0 +1,11 @@
+#ifndef ARMSTRONG_NUMBERS_H
+#define ARMSTRONG_NUMBERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+int getNumberOfDigits(int digit);
+bool is_armstrong_number(int candidate);
+
+#endif
